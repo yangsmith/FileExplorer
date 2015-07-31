0c422b31efb701152b8ddd63607161c078424ae4
syy <1172069959@qq.com>
2015-07-31 16:45:29 +0800

    添加图片

--- src/com/yang/file_explorer/view/SlidingMenuFragment.java
+++ src/com/yang/file_explorer/view/SlidingMenuFragment.java
@@ -7,7 +7,6 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.RelativeLayout;
